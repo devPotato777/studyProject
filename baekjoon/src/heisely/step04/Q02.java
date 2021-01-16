4b51312c4768bc6e53527add3c2994467c47c03a
Heisely <77159506+Heisely@users.noreply.github.com>
2021-01-16 21:57:52 +0900

    백준 알고리즘 4단계 (while문)

--- baekjoon/src/heisely/step04/Q02.java
+++ baekjoon/src/heisely/step04/Q02.java
@@ -8,7 +8,7 @@ public class Q02 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		while (true) {
+		while (scan.hasNext()) { // 런타임 에러방지용으로 scan.hasNext()를 조건으로 달음
 			System.out.print("한 자리 정수를 두 개 입력하세요.(띄어쓰기로 구분)> ");
 			int a = scan.nextInt();
 			int b = scan.nextInt();
