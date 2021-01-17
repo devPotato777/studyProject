31f15710ee42d0e3dd081ba8c43506e562e1f2d3
Scento03 <dlsdud772@gmail.com>
2021-01-17 16:26:05 +0900

    백준 step04

--- tsoj/src/scento03/ch04/Ex03.java
+++ tsoj/src/scento03/ch04/Ex03.java
@@ -6,14 +6,14 @@ public class Ex03 {
 
 	public static void main(String[] args) {
 		int input;
-		
+
 		System.out.print("숫자를 하나 입력하세요.>");
-		
+
 		Scanner scanner = new Scanner(System.in);
 		String tmp = scanner.nextLine();
 		input = Integer.parseInt(tmp);
-		
-		if(input==0) {
+
+		if (input == 0) {
 			System.out.println("입력하신 숫자는 0입니다.");
 		} else {
 			System.out.println("입력하신 숫자는 0이 아닙니다.");
